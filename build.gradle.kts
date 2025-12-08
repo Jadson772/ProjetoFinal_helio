0fdb208c7fc0b05da36550d67fe051cc100a1662
Wilkson32 <164429968+Wilkson32@users.noreply.github.com>
2025-12-08 20:39:28 -0300

    Add files via upload
    Alteração

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
     id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
 }
